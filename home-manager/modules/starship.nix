c69b5ea7c3b582fc75b71dce70d8b5d494c8adfe
wolf4am <wolf4am@gmail.com>
2026-02-09 13:38:55 +0500

    small changes in starship

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -1,35 +1,48 @@
+# {
+#   config,
+#   pkgs,
+#   ...
+# }: {
+#   programs.starship = {
+#     enable = true;
+#     enableZshIntegration = true;
+#     settings = {
+#     add_newline = false;
+
+#     format = "$directory$git_branch$git_status$character";
+
+#     directory = {
+#       style = "white";
+#       truncation_length = 3;
+#       truncation_symbol = "…/";
+#     };
+
+#     git_branch = {
+#       symbol = " ";
+#       style = "bold purple";
+#     };
+
+#     git_status = {
+#       style = "red";
+#     };
+
+#     character = {
+#       success_symbol = "[❯](bold green)";
+#       error_symbol = "[❯](bold red)";
+#     };
+#   };
+#   };
+# }
+
+
+
+{ ... }:
 {
-  config,
-  pkgs,
-  ...
-}: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     settings = {
-    add_newline = false;
-
-    format = "$directory$git_branch$git_status$character";
-
-    directory = {
-      style = "white";
-      truncation_length = 3;
-      truncation_symbol = "…/";
+      battery.disabled = true;
     };
-
-    git_branch = {
-      symbol = " ";
-      style = "bold purple";
-    };
-
-    git_status = {
-      style = "red";
-    };
-
-    character = {
-      success_symbol = "[❯](bold green)";
-      error_symbol = "[❯](bold red)";
-    };
-  };
   };
 }

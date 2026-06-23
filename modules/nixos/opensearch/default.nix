f5c9f9fc55ecfc1d54efafae1f28844fedecc581
wolf4am <kamiltox2006@gmail.com>
2026-06-23 11:35:52 +0500

    chore: change in opensearch module

--- modules/nixos/opensearch/default.nix
+++ modules/nixos/opensearch/default.nix
@@ -5,57 +5,14 @@
   inputs,
   ...
 }:
+
 let
-  # certs_dir = "/var/lib/opensearch/config/certs";
-  cfg = config.services.opensearch;
-  # packages =
-
-  #   pkgs.stdenv.mkDerivation rec {
-  #     pname = "opensearch-dashboards";
-  #     version = "3.5.0";
-
-  #     src = pkgs.fetchurl {
-  #       url = "https://artifacts.opensearch.org/releases/bundle/opensearch-dashboards/${version}/${pname}-${version}-linux-x64.tar.gz";
-  #       hash = "sha256-g0aKKvi2rAd3AFdlfkotzoyREfoSTKJFI7bihjFu2wU=";
-  #     };
-
-  #     patches = [
-  #       # OpenSearch Dashboard specifies that it wants nodejs 14.20.1 but nodejs in nixpkgs is at 14.21.1.
-  #       ./disable-nodejs-version-check.patch
-  #     ];
-
-  #     dontStrip = true;
-
-  #     nativeBuildInputs = [ pkgs.makeWrapper ];
-
-  #     installPhase = ''
-  #       mkdir -p $out/libexec/opensearch-dashboards $out/bin
-  #       mv * $out/libexec/opensearch-dashboards/
-  #       rm -r $out/libexec/opensearch-dashboards/node
-  #       for bin in $out/libexec/opensearch-dashboards/bin/opensearch-dashboards*; do
-  #         makeWrapper $bin $out/bin/$(basename $bin) \
-  #           --prefix PATH : "${
-  #             lib.makeBinPath [
-  #               pkgs.nodejs
-  #               pkgs.coreutils
-  #               pkgs.which
-  #             ]
-  #           }"
-
-  #       done
-  #       rm -rf $out/libexec/opensearch-dashboards/plugins/securityDashboards
-  #     '';
-
-  #     meta = {
-
-  #       cdescription = "Visualization and user interface for OpenSearch";
-  #       homepage = "https://opensearch.org";
-  #       # license = licenses.asl20;
-  #       # platforms = with platforms; linux;
-  #       mainProgram = "opensearch-dashboards";
-  #     };
-  #   };
-
+  opensearch-fixed = pkgs.opensearch.overrideAttrs
+    (final: previous: {
+      installPhase = previous.installPhase + ''
+        chmod +x $out/plugins/opensearch-security/tools/*.sh
+      '';
+    });
 in
 {
   # imports = [ inputs.opensearch-dashboard.nixosModules.default ];
@@ -69,7 +26,7 @@ in
   };
   services.opensearch = {
     enable = true;
-    package = pkgs.opensearch;
+    package = opensearch-fixed;
 
     # extraJavaOptions = [
     #   "-Djna.tmpdir=/var/lib/opensearch/config"
